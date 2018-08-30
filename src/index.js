133782ac755786925585809301337ebb74bf7ba9
spirit <catchspirit37@gmail.com>
2018-08-30 13:56:06 +0800

    z

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 const ModuleUtil = require('./util/ModuleUtil')
 const path = require('path')
 const rootPath = path.resolve(__dirname, '../')
-
 const result = ModuleUtil.requireAll(path.resolve(rootPath, 'src/util'))
+
 Object.freeze(result)
 module.exports = result
