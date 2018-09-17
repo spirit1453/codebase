bb59a6e9177ebaaae9331776b9ec2ddd55324f09
spirit <catchspirit37@gmail.com>
2018-09-17 14:34:37 +0800

    z - Mon Sep 17 2018 14:34:37 GMT+0800 (China Standard Time)

--- index.js
+++ index.js
@@ -59,7 +59,6 @@ module.exports = {
         "no-extra-bind": 2,
         "no-extra-label": 2,
         "no-floating-decimal": 2,
-        "no-var": ["error", { "vars": "all", "args": "after-used", "ignoreRestSiblings": false }],
         "no-template-curly-in-string": 2,
         "eqeqeq": ["error", "always"],
         "no-lone-blocks": 2,
